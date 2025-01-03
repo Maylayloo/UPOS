eb541a6efd526c16ccc1971fbbf9d221cc7624ed
Miłosz <mludwinek@student.agh.edu.pl>
2025-01-03 17:12:07 +0100

    root page structure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,13 +5,21 @@ import Onboarding from "@/app/(root)/(sections)/Onboarding";
 const Root = () => {
     return (
         <section className=''>
-            <h1 className='text-[36px] font-outfit'>
+            <h1 className='text-[36px] font-outfit font-semibold'>
                 UPOS
             </h1>
 
-            <Hero/>
-            <Login/>
-            <Onboarding/>
+            <section className='w-full mt-[5rem]'>
+                <Hero/>
+            </section>
+
+            <section className='w-full bg-green-700 mt-16'>
+                <Login/>
+            </section>
+
+            <section className='w-full bg-blue-700 mt-16'>
+                <Onboarding/>
+            </section>
 
 
         </section>
