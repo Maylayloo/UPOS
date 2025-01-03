e27ccca6769ed27e157a2161a242a27e7bdbe590
Miłosz <mludwinek@student.agh.edu.pl>
2025-01-03 21:26:16 +0100

    footer is prolly done (I hope so)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,28 @@
+import {footerContent} from "@/constants";
+import FooterDiv from "@/components/subcomponents/FooterDiv";
+
 const Footer = () => {
     return (
-        <div>
+        <footer className="bg-[#20201F] text-white pt-12 absolute left-0 right-0">
+            <div className="container mx-auto px-6 grid grid-cols-1 md:grid-cols-3">
+                {
+                    footerContent.map((item, index) => (
+                        <FooterDiv
+                            key={index}
+                            title={item.title}
+                            content={item.content}
+                        />
+                    ))
+                }
 
-        </div>
+
+            </div>
+            <div className="border-t border-gray-700 mt-8 pt-4 text-center text-gray-500 text-sm">
+                © {new Date().getFullYear()} UPOS. Wszelkie prawa zastrzeżone.
+            </div>
+            <div className='w-full h-2 bg-red-50 mt-4 bg-gradient-to-r from-[#183420] to-[#3C3D1F]'/>
+        </footer>
+
     );
 }
 
