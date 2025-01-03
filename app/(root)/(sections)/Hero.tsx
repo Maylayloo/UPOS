5897973abc9577be47b81ad892bb5fedd0e7d4b8
Miłosz <mludwinek@student.agh.edu.pl>
2025-01-03 20:14:45 +0100

    optimization

--- app/(root)/(sections)/Hero.tsx
+++ app/(root)/(sections)/Hero.tsx
@@ -3,18 +3,18 @@ import dawidIMG from "@/public/images/dawid.png"
 
 const Hero = () => {
     return (
-        <section className='flex mx-10 max-lg:flex-col lg:items-center'>
+        <section className='flex mx-10 max-sm:mx-2 max-lg:flex-col lg:items-center'>
             <div className="font-outfit flex-[2]">
-                <h2 className='text-6xl max-md:text-4xl max-lg:w-full font-bold leading-[5rem] w-[75%]'>
+                <h2 className='h2-root'>
                     Witamy w Serwisie UPOS!
                 </h2>
-                <p className='text-xl max-md:text-lg w-[50%] mt-4 max-lg:w-full max-xl:mb-8'>
+                <p className='p-root'>
                     Weź studia w swoje ręce! Zarządzaj planem zajęć, sprawdzaj oceny,
                     zapisuj się na przedmioty, obstawiaj punkty ECTS, przekupuj prowadzących, pnij się w rankingach!
                 </p>
             </div>
-            <div className='flex-1'>
-                <Image className='w-full mix-blend-luminosity'
+            <div className='flex-1 max-lg:flex max-lg:justify-center'>
+                <Image className='mix-blend-luminosity'
                     src={dawidIMG}
                     alt="Witamy!"
                 />
