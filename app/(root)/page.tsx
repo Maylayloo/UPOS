25b9b7bc3f5fc28a0fd107ee0f4122ce10d8b35b
Miłosz <mludwinek@student.agh.edu.pl>
2025-01-04 17:07:31 +0100

    bgd needs improvement in the future

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/app/(root)/(sections)/Hero";
 import Login from "@/app/(root)/(sections)/Login";
 import Onboarding from "@/app/(root)/(sections)/Onboarding";
+import BackgroundDecor from "@/components/subcomponents/BackgroundDecor";
 
 const Root = () => {
     return (
@@ -8,6 +9,12 @@ const Root = () => {
             <h1 className='text-[36px] font-outfit font-semibold'>
                 UPOS
             </h1>
+
+
+            <BackgroundDecor top={-250} left={175} background="#2F7697" hidden={true}/>
+            <BackgroundDecor top={200} left={1200} background="#6E437A" hidden={true}/>
+            <BackgroundDecor top={600} left={-150} background="#464820" hidden={false}/>
+            <BackgroundDecor top={800} left={1000} background="#173B20" hidden={true}/>
 
             <section className='w-full mt-[5rem] font-outfit'>
                 <Hero/>
