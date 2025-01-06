5f0d353f1d8d2e53af6a4398fe5c682df1cf0863
Miłosz <mludwinek@student.agh.edu.pl>
2025-01-06 22:10:14 +0100

    home layout

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Navbar from "@/components/Navbar";
 
 const HomeLayout = ({children, }: {children: React.ReactNode}) => {
+
+    // czy na pewno tak to miało wyglądać???
     return (
+
         <div>
-            { children }
+            <Navbar/>
+             <div className="mb-24 w-full h-1"/>
+
+            <div className="w-full min-h-[75svh] border border-[#9AD6D6] rounded-xl bg-bg p-8 3xl:px-24 mb-16">
+                 {children}
+             </div>
         </div>
     );
 };
