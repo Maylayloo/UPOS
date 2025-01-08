88b0bb84e208525dd6558efc346daca6550c4995
Miłosz <mludwinek@student.agh.edu.pl>
2025-01-08 13:55:18 +0100

    title is dynamic

--- app/(home)/dashboard/layout.tsx
+++ app/(home)/dashboard/layout.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import React from 'react';
+import {usePathname} from "next/navigation";
 //
 // const DashboardLayout = ({student, prof, }: {
 //     student: React.ReactNode;
@@ -8,14 +11,30 @@ import React from 'react';
     //get role from backend
     // const role = "student"
 
+
 const DashboardLayout = ({children,}: {
     children: React.ReactNode;
 }) => {
 
+    const path = usePathname();
+
+    const titles: Record<string, string> = {
+        "/dashboard/profile": "Jan Kowalski", // tu imię i nazwisko zalogowanego użytkownika
+        "/dashboard/schedule": "Plan Zajęć",
+        "/dashboard/statistics": "Statystyki",
+        "/dashboard/grades": "oceny",
+        "/dashboard/groups": "grupy zajęciowe",
+        "/dashboard/roulette": "ruletka",
+    }
+
+    const displayTitle = titles[path] || "panel";
+
+
+
     return (
         <div className='flex flex-col items-center'>
             <h2 className="text-3xl uppercase mt-4 mb-10 tracking-widest font-outfit">
-                panel
+                {displayTitle}
             </h2>
             <div className='flex flex-wrap justify-center gap-8'>
                 {
