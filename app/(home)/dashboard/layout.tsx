f1f637458a41a1921ed2be0f4441c3491babe1a0
Miłosz <mludwinek@student.agh.edu.pl>
2025-01-08 13:26:47 +0100

    came back to children as perfect dad

--- app/(home)/dashboard/layout.tsx
+++ app/(home)/dashboard/layout.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
+//
+// const DashboardLayout = ({student, prof, }: {
+//     student: React.ReactNode;
+//     prof: React.ReactNode;
+// }) => {
 
-const DashboardLayout = ({student, prof, }: {
-    student: React.ReactNode;
-    prof: React.ReactNode;
+    //get role from backend
+    // const role = "student"
+
+const DashboardLayout = ({children,}: {
+    children: React.ReactNode;
 }) => {
 
-    const role = "student"
-
     return (
-        <div>
-          {
-              role === "student" ? student : prof
-          }
+        <div className='flex flex-col items-center'>
+            <h2 className="text-3xl uppercase mt-4 mb-10 tracking-widest font-outfit">
+                panel
+            </h2>
+            <div className='flex flex-wrap justify-center gap-8'>
+                {
+                    children
+                }
+            </div>
         </div>
+
     );
 };
 
