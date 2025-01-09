bbac25c921e2f139dcb2506fd9a5a7151226c4d5
Miłosz <mludwinek@student.agh.edu.pl>
2025-01-09 15:48:04 +0100

    js file storing local user data

---
+++ constants/userProfileData.js
@@ -0,0 +1,26 @@
+export const profileData = [
+    {
+        title: "Email",
+        value: "jkowalski@student.agh.edu.pl"
+    },
+    {
+        title: "Numer Albumu",
+        value: "692137"
+    },
+    {
+        title: "Numer Telefonu",
+        value: "+48 420 213 769",
+        changeable: true,
+    },
+    {
+        title: "Adres",
+        value: "31-056 Kraków, ul. Kupa 4 m. 21",
+        changeable: true,
+    },
+    {
+        title: "Konto Bankowe",
+        value: "69122137721142001123435555",
+        changeable: true,
+    },
+
+]
