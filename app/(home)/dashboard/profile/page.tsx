c5226f1d0f096450f5dba4cec4e802468358ce9c
Miłosz <mludwinek@student.agh.edu.pl>
2025-01-12 02:47:21 +0100

    moved profileData here, data is being fetched from backend and works

--- app/(home)/dashboard/profile/page.tsx
+++ app/(home)/dashboard/profile/page.tsx
@@ -1,11 +1,46 @@
+'use client'
+
 import React from 'react';
 import ProfileField from "@/components/subcomponents/ProfileField";
-import {profileData} from "@/constants/userProfileData";
+import {useUser} from "@/app/(context)/UserContext";
 
 const ProfilePage = () => {
+
+    const {user} = useUser()
+
+
+    // table with user Data provided by { user } from backend
+    const profileData = [
+        {
+            title: "Email",
+            value: user.email
+        },
+        {
+            title: "Numer Albumu",
+            value: user.userId
+        },
+        {
+            title: "Numer Telefonu",
+            value: "+48 420 213 769",
+            changeable: true,
+        },
+        {
+            title: "Adres",
+            value: "31-056 Kraków, ul. Kupa 4 m. 21",
+            changeable: true,
+        },
+        {
+            title: "Konto Bankowe",
+            value: "69122137721142001123435555",
+            changeable: true,
+        },
+
+    ]
+
     return (
         <div className='w-full flex flex-col items-center gap-12 pb-8'>
             {
+                // mapping over profileData table and generating ProfileField components
                 profileData.map(((item, index) => (
                     <ProfileField
                         key={item.title}
