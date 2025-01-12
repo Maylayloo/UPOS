4df8ee2500e5c2028b82b96783e432f2ba49b104
Miłosz <mludwinek@student.agh.edu.pl>
2025-01-12 02:56:00 +0100

    added useUser() hook, getting role from backend, comments

--- app/(home)/dashboard/page.tsx
+++ app/(home)/dashboard/page.tsx
@@ -1,14 +1,22 @@
+'use client'
+
 import {dashboardContent} from "@/constants/dashboardTiles";
 import DashboardTile from "@/components/subcomponents/DashboardTile";
 import React from "react";
+import {useUser} from "@/app/(context)/UserContext";
 
 const Page = () => {
 
-    const role = "student"
+    // useUser() hook
+    const {user} = useUser()
+
+    // get user's role
+    const role = user.rola.toLowerCase();
 
     return (
         <div className="flex flex-wrap justify-center gap-8">
             {
+                // mapping over dashboardContent - table from constants dir, creating tiles using DashboardTile component
                 dashboardContent.map((tile) => (
                     <DashboardTile
                         key={tile.title}
