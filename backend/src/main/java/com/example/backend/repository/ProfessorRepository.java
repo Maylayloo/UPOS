168eda78da45456848b40aebfbd7186ab2074c4c
NotBadKid <dawidm13@interia.pl>
2025-01-16 19:10:17 +0100

    mapping student and professor to user by userId

--- backend/src/main/java/com/example/backend/repository/ProfessorRepository.java
+++ backend/src/main/java/com/example/backend/repository/ProfessorRepository.java
@@ -1,10 +1,11 @@
 package com.example.backend.repository;
 
 import com.example.backend.model.Professor;
+import com.example.backend.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
-
+    Optional<Professor> findByUser_UserId(Long userId);
 }
