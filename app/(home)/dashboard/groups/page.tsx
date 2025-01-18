9dc8e257d7dd3f4ed45e07ccb0996227554535b3
Miłosz <mludwinek@student.agh.edu.pl>
2025-01-19 00:00:26 +0100

    .

--- app/(home)/dashboard/groups/page.tsx
+++ app/(home)/dashboard/groups/page.tsx
@@ -34,7 +34,6 @@ const GroupsPage = () => {
                          courseId={course.courseId}
                      />
                  ))
-
                 }
             </div>
         </div>
