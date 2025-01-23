cd896745fa319748c4fa504772e5f8fbcffcad82
Miłosz <mludwinek@student.agh.edu.pl>
2025-01-23 12:49:45 +0100

    Link/push to 'group' page

--- components/subcomponents/GroupContainer.tsx
+++ components/subcomponents/GroupContainer.tsx
@@ -1,20 +1,30 @@
+import Link from "next/link";
+
 interface groupsInterface {
     type: string,
     no: number, // group number
     dotw: string, // day of the week
     startHour: string,
     endHour: string,
+    courseName: string,
+    groupId: number
 }
 
-const GroupContainer = ({type, no, dotw, startHour, endHour}: groupsInterface) => {
+const GroupContainer = ({type, no, dotw, startHour, endHour, courseName, groupId}: groupsInterface) => {
     return (
         <div className='px-4 flex flex-col py-1 mt-1'>
             <div>
-                <h2 className='font-roboto font-[500]'>
-                    &#x2022; {type}, <span className='font-[300]'>
+                <Link
+                    href={`/dashboard/groups/${courseName}/${type}/nr_grupy=${no}_&id=${groupId}`}
+                    className='hover:underline'
+
+                >
+                    <h2 className='font-roboto font-[500]'>
+                        &#x2022; {type}, <span className='font-[300]'>
                     grupa nr {no}
                 </span>
-                </h2>
+                    </h2>
+                </Link>
                 <h3 className='px-2.5'>
                     {dotw}, {startHour} - {endHour}
                 </h3>
