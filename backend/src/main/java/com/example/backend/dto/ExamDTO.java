572c873008ad0c58b0149324be27f9e07e0bdb5f
NotBadKid <dawidm13@interia.pl>
2025-01-24 17:23:52 +0100

    added getters and setters, lombok didn't work again :<

--- backend/src/main/java/com/example/backend/dto/ExamDTO.java
+++ backend/src/main/java/com/example/backend/dto/ExamDTO.java
@@ -7,6 +7,30 @@ import lombok.Setter;
 @Setter
 public class ExamDTO {
     private Long courseId;
+    private Long professorId;
     private String date;
     private String place;
+    private String attempt;
+
+    public Long getCourseId() {return courseId;}
+
+    public void setCourseId(Long courseId) {this.courseId = courseId;}
+
+    public Long getProfessorId() {return professorId;}
+
+    public void setProfessorId(Long professorId) {this.professorId = professorId;}
+
+    public String getDate() {return date;}
+
+    public void setDate(String date) {this.date = date;}
+
+    public String getPlace() {return place;}
+
+    public void setPlace(String place) {this.place = place;}
+
+    public String getAttempt() {return attempt;}
+
+    public void setAttempt(String attempt) {this.attempt = attempt;}
 }
+
+
