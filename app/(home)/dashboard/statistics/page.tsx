6b1032dcec618c6a865ca96f9a124af5b8e4edf2
Miłosz <mludwinek@student.agh.edu.pl>
2025-01-24 14:29:26 +0100

    /statistics page is done

--- app/(home)/dashboard/statistics/page.tsx
+++ app/(home)/dashboard/statistics/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import Image from 'next/image'
+import statsPNG from '@/public/images/statm.jpg'
 
 const StatsPage = () => {
     return (
-        <div>
-            Stats
+        <div className='flex justify-center mb-8'>
+            <Image
+                className='rounded-xl border border-yellow-500 shadow-yellow-600 shadow-2xl  '
+                src={statsPNG}
+                alt="Statystyki"
+                width={400}
+            />
         </div>
     );
 };
