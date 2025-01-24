b61af9a81d6b9784f15c6917060a3ccd2ae8a9b4
aloix123 <69531892+aloix123@users.noreply.github.com>
2025-01-24 13:33:44 +0100

    created some student api ,added students to course

--- backend/src/main/java/com/example/backend/dto/StudentCourseDTO.java
+++ backend/src/main/java/com/example/backend/dto/StudentCourseDTO.java
@@ -3,9 +3,15 @@ package com.example.backend.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
 public class StudentCourseDTO {
     int semester;
     String major;
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public String getMajor() {
+        return major;
+    }
 }
