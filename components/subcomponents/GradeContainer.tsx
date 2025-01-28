7251737b8398299f41b4e608d33b3310e197f3c3
Miłosz <mludwinek@student.agh.edu.pl>
2025-01-28 20:47:14 +0100

    changed props structure

--- components/subcomponents/GradeContainer.tsx
+++ components/subcomponents/GradeContainer.tsx
@@ -1,15 +1,16 @@
 interface props {
     type: string,
-    grade: string,
 }
 
-const GradeContainer = ({type, grade}: props) => {
+const GradeContainer = ({type}: props) => {
+    const grade = "-"
+
     return (
         <div className="flex justify-between items-center ">
             <h2>
                 {type}
             </h2>
-            <div className={`rounded-full w-[3rem] h-[3rem] relative shadow-md flex justify-center items-center ${grade === '-' ? 'text-2xl' : 'bg-[#67095F]'}`}>
+            <div className={`rounded-full w-[3rem] h-[3rem] relative shadow-md flex justify-center items-center ${grade === "-" ? 'text-2xl' : 'bg-[#67095F]'}`}>
                 <h3 className=''>
                     {grade}
                 </h3>
