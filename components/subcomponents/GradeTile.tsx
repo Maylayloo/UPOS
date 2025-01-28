f17619841cb3e3071e6363b5eb2ade1d57f6faaf
Miłosz <mludwinek@student.agh.edu.pl>
2025-01-28 20:31:58 +0100

    added props

--- components/subcomponents/GradeTile.tsx
+++ components/subcomponents/GradeTile.tsx
@@ -1,13 +1,19 @@
 import GradeContainer from "@/components/subcomponents/GradeContainer";
 
-const GradeTile = () => {
+interface props {
+    courseName: string,
+    ects: number,
+    courseId: string,
+}
+
+const GradeTile = ({courseName, ects, courseId}: props) => {
     return (
         <div className='font-roboto px-6 pt-4 pb-8 w-[27.5%] border rounded-xl'>
             <h1 className='text-lg'>
-                Metody Numeryczne
+                {courseName}
             </h1>
             <span className='font-[300]'>
-                punkty ECTS: 4.00
+                punkty ECTS: {ects}.00
             </span>
             <div className='flex flex-col gap-4 mt-2'>
                 <GradeContainer type='Przedmiot' grade='3.5'/>
