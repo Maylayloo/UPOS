832b1d3384f7d050556ecc8394410b9e910e9be3
Miłosz <mludwinek@student.agh.edu.pl>
2025-01-29 14:21:12 +0100

    ExamTile skeleton

---
+++ components/subcomponents/ExamTile.tsx
@@ -0,0 +1,26 @@
+const ExamTile = () => {
+    return (
+        <div className="border font-roboto min-w-max p-4">
+            <h1 className='text-lg'>
+                Metody Numeryczne
+            </h1>
+            <h2>
+                Termin: <span className='font-[300]'>
+                0
+            </span>
+            </h2>
+            <h3>
+                Data: <span className='font-[300]'>
+                11.02.2025 16:00
+            </span>
+                <br/>
+                Miejsce: <span className='font-[300]'>
+                s.201 bud. D6
+            </span>
+            </h3>
+
+        </div>
+    );
+};
+
+export default ExamTile;
