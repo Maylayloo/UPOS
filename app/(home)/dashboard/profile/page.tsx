38782d65c68a998947e27b8fa27871f20ae82afa
Miłosz <mludwinek@student.agh.edu.pl>
2025-01-30 19:33:53 +0100

    data you see depends on your role now

--- app/(home)/dashboard/profile/page.tsx
+++ app/(home)/dashboard/profile/page.tsx
@@ -8,31 +8,42 @@ const ProfilePage = () => {
 
     // change localstorage to session storage
     const storedUser = JSON.parse(localStorage.getItem("upos_user") || '{}');
+    const profTitle = JSON.parse(localStorage.getItem("upos_prof_title") as string);
+
     // table with user Data provided by { user } from backend
     const profileData = [
         {
+            title: "Tytuł Naukowy",
+            value: profTitle,
+            visibility: ["professor"]
+        },
+        {
             title: "Email",
-            value: storedUser.email
+            value: storedUser.email,
+            visibility: ["professor", "student"]
         },
         {
             title: "Numer Albumu",
-            value: storedUser.userId
+            value: storedUser.userId,
+            visibility: ["student"]
         },
         {
             title: "Numer Telefonu",
             value: storedUser.number,
             changeable: true,
+            visibility: ["professor", "student"]
         },
         {
             title: "Adres",
             value: storedUser.address,
-            // value: "31-056 Kraków, ul. Kupa 4 m. 21",
             changeable: true,
+            visibility: ["professor", "student"]
         },
         {
             title: "Konto Bankowe",
             value: storedUser.bankNumber,
             changeable: true,
+            visibility: ["professor", "student"]
         },
 
     ]
@@ -48,6 +59,7 @@ const ProfilePage = () => {
                         title={item.title}
                         value={item.value}
                         {...(item.changeable ? {changeable: true} : {})}
+                        visibility={item.visibility}
                     />
                 )))
             }
--- components/subcomponents/ProfileField.tsx
+++ components/subcomponents/ProfileField.tsx
@@ -7,12 +7,16 @@ interface profileFieldProps {
     title: string,
     value: string,
     id: string,
+    visibility?: string[]
 }
 
-const ProfileField = ({changeable, title, value, id}: profileFieldProps) => {
+const ProfileField = ({changeable, title, value, id, visibility}: profileFieldProps) => {
 
     const [editable, setEditable] = useState(false);
     const [saveButton, setSaveButton] = useState(false);
+    const storedUser = JSON.parse(localStorage.getItem("upos_user") || '{}');
+    const role = storedUser.role.toLowerCase();
+
 
     const edit = () => {
 
@@ -34,51 +38,52 @@ const ProfileField = ({changeable, title, value, id}: profileFieldProps) => {
         // and probably change something to form, and types, you know
     }
 
+    if (visibility?.includes(role)) {
+        return (
+            <div className="w-3/4 border-2 border-[#C1C1C1] rounded-xl relative font-roboto">
+                <div className="absolute left-12 -top-3 border-2 border-[#C1C1C1] px-12 bg-bg rounded-lg text-sm">
+                    {title}
+                </div>
 
-    return (
-        <div className="w-3/4 border-2 border-[#C1C1C1] rounded-xl relative font-roboto">
-            <div className="absolute left-12 -top-3 border-2 border-[#C1C1C1] px-12 bg-bg rounded-lg text-sm">
-                { title }
+                {
+                    changeable ? (
+                        <input
+                            id={id}
+                            type="text"
+                            defaultValue={value}
+                            className='bg-bg w-full px-8 py-[1.2rem] text-lg tracking-wide rounded-xl font-[200]'
+                            readOnly={!editable}
+                        />
+                    ) : (
+                        <input
+                            type="text"
+                            defaultValue={value}
+                            className='bg-bg w-full px-8 py-[1.2rem] text-lg tracking-wide rounded-xl font-[200]'
+                            readOnly
+                        />
+                    )
+                }
+
+
+                {changeable && (
+                    saveButton ? (
+                        <button onClick={save}
+                                className='uppercase tracking-wider absolute rounded-xl text-bg bg-[#C1C1C1] px-12 py-1 font-bold right-8 -bottom-4 font-outfit'
+                        >
+                            Zapisz
+                        </button>
+                    ) : (
+                        <button onClick={edit}
+                                className='uppercase tracking-wider absolute rounded-xl text-bg bg-[#C1C1C1] px-12 py-1 font-bold right-8 -bottom-4 font-outfit'
+                        >
+                            Zmień
+                        </button>
+                    )
+
+                )}
             </div>
-
-            {
-                changeable ? (
-                    <input
-                        id={id}
-                        type="text"
-                        defaultValue={value}
-                        className='bg-bg w-full px-8 py-[1.2rem] text-lg tracking-wide rounded-xl font-[200]'
-                        readOnly={!editable}
-                    />
-                ) : (
-                    <input
-                        type="text"
-                        defaultValue={value}
-                        className='bg-bg w-full px-8 py-[1.2rem] text-lg tracking-wide rounded-xl font-[200]'
-                        readOnly
-                    />
-                )
-            }
-
-
-            {changeable && (
-                saveButton ? (
-                    <button onClick={save}
-                            className='uppercase tracking-wider absolute rounded-xl text-bg bg-[#C1C1C1] px-12 py-1 font-bold right-8 -bottom-4 font-outfit'
-                    >
-                        Zapisz
-                    </button>
-                ) : (
-                    <button onClick={edit}
-                            className='uppercase tracking-wider absolute rounded-xl text-bg bg-[#C1C1C1] px-12 py-1 font-bold right-8 -bottom-4 font-outfit'
-                    >
-                        Zmień
-                    </button>
-                )
-
-            )}
-        </div>
-    );
+        );
+    }
 };
 
 export default ProfileField;
