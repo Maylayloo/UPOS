c53e5dbc6ee3c8320b3aa8dc0e771058f5c4f4a0
Miłosz <mludwinek@student.agh.edu.pl>
2025-02-02 11:49:18 +0100

    prof now see students' grades (if exist)

--- components/subcomponents/StudentInGroupContainer.tsx
+++ components/subcomponents/StudentInGroupContainer.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import React, {useEffect, useState} from "react";
+import Loading from "@/components/Loading";
 
 interface Props {
     index: number;
@@ -12,9 +13,39 @@ const StudentInGroupContainer = ({ index, name, groupId, studentId}: Props) => {
     const role = storedUser.role.toLowerCase();
     const grades = ["2.0", "3.0", "3.5", "4.0", "4.5", "5.0"];
 
+
     const [activeGrade, setActiveGrade] = useState(false);
     const [selectedGrade, setSelectedGrade] = useState(grades[0]);
     const [successfullyGraded, setSuccessfullyGraded] = useState(false);
+    const [alreadyGraded, setAlreadyGraded] = useState(false);
+    const [loading, setLoading] = useState(true);
+    let [currentGrade, setCurrentGrade] = useState("");
+
+    useEffect(() => {
+
+        const fetchGrade = async () => {
+            try {
+                const response = await fetch(`http://localhost:8080/students/${studentId}/grades/NonPartial/${groupId}`, {
+                    method: 'GET',
+                    credentials: "include",
+                    });
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const fetchedGrade = await response.json();
+                setCurrentGrade(fetchedGrade.value)
+                setAlreadyGraded(true);
+
+            } catch(err) {
+                console.log("prawdopodobnie nie znaleziono oceny", studentId)
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchGrade();
+    }, [])
+
+
 
     // grade student
     const handleSubmit = async () => {
@@ -45,6 +76,11 @@ const StudentInGroupContainer = ({ index, name, groupId, studentId}: Props) => {
         }
     };
 
+    if (loading) {
+     return (
+         <Loading/>
+     )
+    }
     if (role === "student") {
         return (
             <div className="border-b border-b-[#DBE3D4] w-full font-roboto tracking-wide font-[300] py-1 flex justify-center">
@@ -55,14 +91,29 @@ const StudentInGroupContainer = ({ index, name, groupId, studentId}: Props) => {
             </div>
         );
     }
-
+    // if you're prof or admin
     return (
         <div className="border-b border-b-[#DBE3D4] w-full font-roboto tracking-wide font-[300] py-1 flex justify-center gap-16 items-center">
             <h2>
                 {index}. {name}
             </h2>
             <div>
-                {!activeGrade && !successfullyGraded && (
+                { alreadyGraded && (
+                    <div className='flex items-center gap-4 -ml-6 font-roboto font-[400]'>
+                        {currentGrade}
+                        <button
+                            onClick={() => {}}
+                            className="bg-[#DBE3D4] text-bg font-[500] font-roboto my-1 px-3 rounded-xl hover:bg-bg hover:text-[#DBE3D4] border-2 border-bg hover:border-[#DBE3D4]"
+                        >
+                            ZMIEŃ
+                        </button>
+                    </div>
+
+                )
+
+                }
+
+                {!activeGrade && !successfullyGraded && !alreadyGraded && (
                     <button
                         onClick={() => setActiveGrade(true)}
                         className="bg-[#DBE3D4] text-bg font-[500] font-roboto my-1 px-3 rounded-xl hover:bg-bg hover:text-[#DBE3D4] border-2 border-bg hover:border-[#DBE3D4]"
