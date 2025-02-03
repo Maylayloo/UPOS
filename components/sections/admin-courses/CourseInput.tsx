b4145049d7dcae737a3878175de6086709968784
Miłosz <mludwinek@student.agh.edu.pl>
2025-02-03 18:16:13 +0100

    added name prop and turned off autocompletion

--- components/sections/admin-courses/CourseInput.tsx
+++ components/sections/admin-courses/CourseInput.tsx
@@ -2,9 +2,10 @@ interface props {
     title: string,
     placeholder: string,
     width?: string,
+    name: string,
 }
 
-const CourseInput = ({title, placeholder, width}: props) => {
+const CourseInput = ({title, placeholder, width, name}: props) => {
     return (
         <div className={`flex flex-col items-center w-[${width}]`}>
             <h2 className='mb-1 font-roboto '>
@@ -14,6 +15,8 @@ const CourseInput = ({title, placeholder, width}: props) => {
                 className='text-black text-lg focus:outline-none px-5 py-2 rounded-xl w-full focus:bg-blue-100 placeholder:text-base'
                 type="text"
                 placeholder={`np. ${placeholder}`}
+                name={name}
+                autoComplete="off"
             />
         </div>
     );
