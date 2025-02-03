e34e09716ede78ee06192b332b2916af2cc4820a
Miłosz <mludwinek@student.agh.edu.pl>
2025-02-03 18:21:40 +0100

    success page shown after creating new course as admin

---
+++ app/(home)/dashboard/course-management/create/success/page.tsx
@@ -0,0 +1,29 @@
+import ChoiceButton from "@/components/sections/admin-courses/ChoiceButton";
+import createIcon from "@/public/icons/create.png"
+import previousIcon from "@/public/icons/previous.png"
+
+const Page = () => {
+    return (
+        <div className="text-center flex items-center flex-col">
+            <h1 className='text-xl font-roboto'>
+                Udało Ci się pomyślnie utworzyć nowy przedmiot!
+            </h1>
+            <div className="w-full flex flex-col items-center gap-4 mt-8">
+                <ChoiceButton
+                    title="Tworzę kolejny"
+                    icon={createIcon}
+                    alt="Stwórz"
+                    href='.'
+                />
+                <ChoiceButton
+                    title="Powrót"
+                    icon={previousIcon}
+                    alt="Powrót"
+                    href='/dashboard'
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Page;
