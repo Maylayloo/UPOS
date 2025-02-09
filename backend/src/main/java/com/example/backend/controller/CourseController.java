8e2f156f1ef8c2e2cfea4f0c6a7bc6b7e9fd5088
aloix123 <pawel.kupis@o2.pl>
2025-02-09 14:47:14 +0100

    get courses by major and semester done !!!!!!!!

--- backend/src/main/java/com/example/backend/controller/CourseController.java
+++ backend/src/main/java/com/example/backend/controller/CourseController.java
@@ -1,5 +1,7 @@
 package com.example.backend.controller;
 
+import com.example.backend.dto.RequestSpecyficCourseByMajorAndSemesterDTO;
+import com.example.backend.service.adminService.CourseManagementForAdminService;
 import com.example.backend.service.professorService.CourseManagementForProfessorService;
 import com.example.backend.service.studentService.CourseForStudentService;
 import com.example.backend.service.userService.UserWebAuthenticationService;
@@ -7,9 +9,7 @@ import com.example.backend.util.UserUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/courses")
@@ -20,7 +20,8 @@ public class CourseController {
     private CourseManagementForProfessorService courseManagementForProfessorService;
     @Autowired
     private UserWebAuthenticationService userWebAuthenticationService;
-
+    @Autowired
+    private CourseManagementForAdminService courseManagementForAdminService;
     @GetMapping("/loggedIn")
     public ResponseEntity<?> loggedIn() {
         if(userWebAuthenticationService.isLoggedInStudent()){
@@ -38,5 +39,10 @@ public class CourseController {
     @GetMapping("/")
     public ResponseEntity<?> getAllCourses() {
         return new ResponseEntity<>(courseManagementForProfessorService.getAllCourses(),HttpStatus.OK);
+    }
+
+    @GetMapping("/majorsAndSemesters")
+    public ResponseEntity<?> getCoursesByMajorAndSemester(@RequestBody RequestSpecyficCourseByMajorAndSemesterDTO dto) {
+        return new ResponseEntity<>(courseManagementForAdminService.getAllCoursesByMajorAndSemester(dto.getMajor(), dto.getSemester()),HttpStatus.OK);
     }
 }
---
+++ backend/src/main/java/com/example/backend/dto/RequestSpecyficCourseByMajorAndSemesterDTO.java
@@ -0,0 +1,14 @@
+package com.example.backend.dto;
+
+public class RequestSpecyficCourseByMajorAndSemesterDTO {
+    private String major;
+   private String semester;
+
+    public String getMajor() {
+        return major;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+}
