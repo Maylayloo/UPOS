74699e523f67fa6af218ad3ed2096fec8449dd87
Miłosz <mludwinek@student.agh.edu.pl>
2025-02-10 11:39:24 +0100

    page for managing existing groups as admin, blank for now

---
+++ app/(home)/dashboard/group-management/manage/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div className='font-roboto text-2xl justify-center w-full text-center'>
+            Nic tu jeszcze nie ma :(
+        </div>
+    );
+};
+
+export default Page;
